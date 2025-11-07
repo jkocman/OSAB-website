6e340569971acd890879934ec08600387b1772b3
Krooby <jkocman07@gmail.com>
2025-11-07 21:39:29 +0100

    feat(frontend): finished the welcome section

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,34 +1,76 @@
 <template>
   <main>
     <section>
-      <p>asdasdas</p>
       <h1>Open <span>Shapes</span> & <span>Beats</span></h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit officia perferendis maiores
+        facere perspiciatis asperiores, vero inventore consequuntur omnis, voluptate.
+      </p>
+      <section class="button-container">
+        <Button
+          title="Download"
+          :fontSize="22"
+          :paddingHorizontal="35"
+          :paddingVertical="10"
+          buttonType="primary"
+          @click="router.push('/download')"
+        ></Button>
+        <Button
+          title="Learn more"
+          :fontSize="22"
+          :paddingHorizontal="35"
+          :paddingVertical="10"
+          buttonType="reverse"
+          @click="router.push('/about')"
+        ></Button>
+      </section>
     </section>
   </main>
 </template>
 
+<script lang="ts" setup>
+import { useRoute, useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
+
 <style lang="scss" scoped>
 main {
-  section {
+  section:first-child {
     background:
       linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('../assets/img/osab.jpeg');
     background-size: cover;
     background-position: top;
     background-repeat: no-repeat;
-    height: 800px;
+    height: 900px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    gap: 30px;
     text-align: center;
     color: white;
 
     h1 {
-      font-size: 3rem;
+      font-weight: 500;
+      font-size: var(--xl-text-size);
     }
-
+    span:first-child {
+      color: var(--secondary-foreground-color);
+    }
     span:nth-child(2) {
-      color: #ff3366;
+      color: var(--primary-foreground-color);
+    }
+    p {
+      width: 1200px;
+      font-size: var(--medium-text-size);
+    }
+    .button-container{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 40px;
+      margin-top: 10px;
     }
   }
 }
