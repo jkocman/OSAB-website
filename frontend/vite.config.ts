f7d0e6da1e4c7ab988e06d2571128d8b20baa5e7
Krooby <jkocman07@gmail.com>
2026-02-15 10:55:53 +0100

    fix(frontend): change of path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/OSAB-website/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
